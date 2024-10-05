5efefaa3025b8ecdac81d253da88626852915f5e
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2024-10-05 11:56:43 +0530

    logo-and-heade-addedr

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import brandLogo from "../src/assets/BrandLogo.svg";
+import searchIcon from "../src/assets/SearchIcon.svg";
+
 
 function App() {
   return (
     <div className="App">
+      <div className="brand-logo-container">
+         <img src={brandLogo} alt="brandLogo"/>
+      </div>
+      <div className="header">
+            <div className="header-title">
+                <h6 className="header-title-top">Cameras</h6>
+                <h6 className="header-title-bottom">Manage your cameras here</h6>
+            </div>
+            <div className="search-box-container">
+                <input className="search-box" placeholder="search"/>
+                <img src={searchIcon} alt="searchIcon" className="search-icon"/>
+            </div>
+      </div>
     </div>
   );
 }
